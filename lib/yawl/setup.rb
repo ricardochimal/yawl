0a0a2e185b395679c6a302b3e1953b73ffdd99b5
Ricardo Chimal, Jr <kiwi@null.cx>
2014-01-27 23:11:11 -0800

    added Yawl::Setup, trying this out...

---
+++ lib/yawl/setup.rb
@@ -0,0 +1,57 @@
+require 'sequel'
+
+Sequel.extension :migration
+
+module Yawl
+  module Setup
+    extend self
+
+    MIGRATION = Sequel.migration do
+      change do
+        create_table(:processes) do
+          primary_key :id
+          String :desired_state, :text=>true, :null=>false
+          String :state, :default=>"pending", :text=>true, :null=>false
+          DateTime :created_at
+          String :name, :text=>true
+          String :config
+          String :request_id, :text=>true
+          String :specified_attributes
+        end
+
+        create_table(:steps) do
+          primary_key :id
+          Integer :process_id, :null=>false
+          Integer :seq, :null=>false
+          String :name, :text=>true, :null=>false
+          String :state, :default=>"pending", :text=>true, :null=>false
+
+          index [:process_id]
+        end
+
+        create_table(:step_attempts) do
+          primary_key :id
+          Integer :step_id, :null=>false
+          File :output
+          DateTime :started_at
+          DateTime :completed_at
+
+          index [:step_id]
+        end
+      end
+    end
+
+
+    def create!
+      migration.apply(db, :up)
+    end
+
+    def destroy!
+      migration.apply(db, :down)
+    end
+
+    def db
+      Sequel.connect(ENV["DATABASE_URL"])
+    end
+  end
+end
