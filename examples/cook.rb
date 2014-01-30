d4f2eeb322a55515a42f6c4d32fe8348ff6d6dd0
Ricardo Chimal, Jr <kiwi@null.cx>
2014-01-30 13:27:11 -0800

    update examples

--- examples/cook.rb
+++ examples/cook.rb
@@ -1,8 +1,6 @@
 $:.unshift(File.expand_path(File.join(File.dirname($0), "../lib")))
 
-require "sequel"
-
-Sequel.connect(ENV["DATABASE_URL"] || "postgres://localhost/yawl_examples")
+ENV["DATABASE_URL"] ||= "postgres://localhost/yawl_examples"
 
 require "yawl"
 require File.dirname(File.expand_path(__FILE__)) + "/steps/scrambled_eggs"
--- examples/cook_worker.rb
+++ examples/cook_worker.rb
@@ -2,8 +2,7 @@
 
 $:.unshift(File.expand_path(File.join(File.dirname($0), "../lib")))
 
-require "sequel"
-Sequel.connect(ENV["DATABASE_URL"] || "postgres://localhost/yawl_examples")
+ENV["DATABASE_URL"] ||= "postgres://localhost/yawl_examples"
 
 require "yawl"
 require "yawl/worker"
