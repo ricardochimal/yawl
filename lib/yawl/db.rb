fc9f40a2ff52864a0eb3b85ea287da909e126714
Ricardo Chimal, Jr <kiwi@null.cx>
2014-02-25 13:31:18 -0800

    yawl/db: split out some actions to Yawl.setup_database_options, useful when sharing connections

--- lib/yawl/db.rb
+++ lib/yawl/db.rb
@@ -3,11 +3,16 @@ require 'yawl/config'
 
 module Yawl
   DB = Sequel.connect(ENV["DATABASE_URL"].gsub('postgresql://', 'postgres://'))
-  DB.extension :pg_json
-  DB.execute("SET bytea_output TO 'escape'")
 
   if Config.log_sequel?
     require 'logger'
     DB.loggers << Logger.new($stdout)
   end
+
+  def self.setup_database_options
+    DB.extension :pg_json
+    DB.execute("SET bytea_output TO 'escape'")
+  end
 end
+
+Yawl.setup_database_options
